cbab87f8ea82e1f67c7611559609f42e8a856cab
atshaya-anand <atshaya1699@gmail.com>
2022-01-21 21:19:10 +0530

    added changes in test file and modified gradle

--- src/test/java/PowerFinderTest.java
+++ src/test/java/PowerFinderTest.java
@@ -1,26 +1,24 @@
-package powerpackage;
-
-
 import org.junit.Test;
+import powerpackage.PowerFinder;
 
-public class powertest {
+public class PowerFinderTest {
     @Test
     public void one_raised_to_one_is_onew() {
-        assert powerfinder.OF(1,1) == 1;
+        assert PowerFinder.OF(1,1) == 1;
     }
 
     @Test
     public void twoRaised_to_one_is_2() {
-        assert powerfinder.OF(2,1) == 2;
+        assert PowerFinder.OF(2,1) == 2;
     }
 
     @Test
     public void two_power_2_is_4() {
-        assert powerfinder.OF(2, 2) == 4;
+        assert PowerFinder.OF(2, 2) == 4;
     }
 
     @Test
     public void power_of_2_and_3_Is_SIX() {
-        assert powerfinder.OF(3, 2) == 3*3;
+        assert PowerFinder.OF(3, 2) == 3*3;
     }
 }
